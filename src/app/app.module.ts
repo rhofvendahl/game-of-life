27cdc111eb6479f470c5e5dd8731226f08c5f4b8
Guest User <Guest@Epicodus-3A.local>
2018-03-29 16:37:00 -0700

    remove library component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
-import { LibraryComponent } from './library/library.component';
+// import { LibraryComponent } from './library/library.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    LibraryComponent
+    // LibraryComponent
   ],
   imports: [
     BrowserModule
